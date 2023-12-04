51c5da5a609d20dd5410dad01b8005a20b0596d6
Metacraft <rafy.vec@gmail.com>
2023-12-04 15:11:26 +0100

    Fonction insereArbre et pseudo code Huffman

--- rafael.py
+++ rafael.py
@@ -12,12 +12,36 @@ def insere(file:list,arbre) :
     """
     Insere un arbre dans une file à priorités
     """
-    return None #Rafael
+    for i in range(len(file)):  
+        if arbre.poids > file[i].poids and arbre.poids < file[i+1].poids :
+            file.insert(i+1, arbre)
 
 
 def Huffman(texte:str) :
     """
-    Crée l'arbre de Huffman
+    données : une texte
+    résultat : l’arbre binaire du codage de Huffman 
     """
-    return None #Rafael
+    pass
 
+
+"""
+Fonction Huffman(texte) → arbre
+    données : une texte
+    résultat : l’arbre binaire du codage de Huffman
+    Algorithme
+        créer un dictionnaire dico avec les fréquences de chaque lettre
+        créer une file à priorités
+        pour chaque caractère c de dico faire
+            créer un arbre a
+            a.caractère ← c
+            a.poids ← fréquence de c
+            ajouter a à la file à priorités
+        tant que la file à priorités contient plus d’un arbre faire
+            créer un arbre a
+            a.gauche ← l’arbre de poids le plus faible de la file à priorités (que l’on retire de la file)
+            a.droite ← l’arbre de poids le plus faible de la file à priorités (que l’on retire de la file)
+            a.poids ← a.gauche.poids + a.droite.poids
+            ajouter a à la file à priorités
+        retourner le seul arbre contenu dans la file à priorités
+"""
